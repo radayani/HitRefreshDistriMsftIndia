f9524fa525bf0e62b143381f8d91bf4508429a8d
Raveena Dayani <radayani@microsoft.com>
2017-09-20 19:35:45 +0530

    added building into the api call. and updated the db data to handle book issue from any location.

--- project/src/App.js
+++ project/src/App.js
@@ -66,7 +66,7 @@ class App extends Component {
     console.log(this.state.empId);
 
     if (this.state.locationDefault !== -1 && this.state.buildingDefault !== -1 && this.state.empId !== null) {
-      fetch(`/api/validate?id=${this.state.empId}&city=${this.state.locationDefault}`, {
+      fetch(`/api/validate?id=${this.state.empId}&location=${this.state.locationDefault}&building=${this.state.buildingDefault}`, {
 
         headers: {
           'Content-Type': 'application/json',
