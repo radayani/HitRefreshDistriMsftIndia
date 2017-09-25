7bbeeb3a57b571526fad1aeb86871ded0fe9d6c8
Raveena Dayani <radayani@microsoft.com>
2017-09-25 16:56:39 +0530

    changes

--- app.js
+++ app.js
@@ -61,8 +61,8 @@ app.get('/api/validate', (req, res, err) => {
           if (!err) {
             // result contains the entity
             if (result) {
-              if (result.Received['_'] == true) {                
-                console.log(`book has already been collected by the employee EmployeeId:${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.building }`);
+              if (result.Received['_'] == true) {
+                console.log(`book has already been collected by the employee EmployeeId:${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.building}`);
                 res.status(409).json({ "message": `Book Already Collected for Employee: ${result.Alias['_']}` }).end();
               }
               else {
@@ -76,7 +76,7 @@ app.get('/api/validate', (req, res, err) => {
                 }
                 tableService.mergeEntity('employees', task, function (err) {
                   if (!error) {
-                    console.log(`book has been provided to the employee EmployeeId:${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.building }`);
+                    console.log(`book has been provided to the employee EmployeeId:${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.building}`);
 
                     res.status(200).json({ "message": `Confirm Name & then Provide the Book to ${result.Alias['_']}` }).end();
 
@@ -90,8 +90,8 @@ app.get('/api/validate', (req, res, err) => {
                 });
               }
             }
-            else{
-              console.log(`Result not created. error:  EmployeeId: ${req.query.id} \n IssueLocation: ${req.query.location} IssueBuilding: ${req.query.building }`);
+            else {
+              console.log(`Result not created. error:  EmployeeId: ${req.query.id} \n IssueLocation: ${req.query.location} IssueBuilding: ${req.query.building}`);
             }
           }
           else {
@@ -129,8 +129,15 @@ app.get('/api/login', (req, res, err) => {
             // result contains the entity
             if (result) {
               //console.log(result);
-              {
+              if (result.Location['_'] !== 'null' && result.Building['_'] !== 'null') {
+                console.log(`Issuer id already in use.  issuer Id: ${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.Building}`);
+                res.status(300).json({ "message": "issuer id already in use" }).end();
+
+              }
+              else {
                 console.log("FouND!!");
+                console.log(result.Location);
+
                 //console.log(result.Received['_']);
                 //console.log("is this getting printed");
                 var entGen = azure.TableUtilities.entityGenerator;
@@ -141,11 +148,13 @@ app.get('/api/login', (req, res, err) => {
                   Location: entGen.String(req.query.location),
                   Building: entGen.String(req.query.Building)
                 }
-                tableService.mergeEntity('distributors', task, function (err, result, response) {
+                tableService.mergeEntity('distributors', task, function (err, reslt, response) {
                   if (!error) {
-                    
-                    console.log(`Entry updated in Db for the issue address. Login Success. IssuerId:${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.building }`);
+
+
+                    console.log(`Entry updated in Db for the issue address. Login Success. IssuerId:${req.query.id} \n IssueLocation: ${req.query.location} \n IssueBuilding: ${req.query.building}`);
                     res.status(200).json({ "message": "Login Success!" }).end();
+
 
                   }
                   else {
